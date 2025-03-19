4a6373a88677e7852812e72461887c46d5040e48
tmerowr <tmerowr@gmail.com>
2025-03-19 10:16:28 +0800

    Site updated: 2025-03-19 10:16:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/18/hello-world/"];function toRandomPost(){
+var posts=["2025/03/19/2/","2025/03/18/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
