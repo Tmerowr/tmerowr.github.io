0fd3f5d0823149103c329099c4fe3f5eb4735305
tmerowr <tmerowr@gmail.com>
2025-03-19 11:21:58 +0800

    Site updated: 2025-03-19 11:21:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/19/2/","2025/03/18/hello-world/"];function toRandomPost(){
+var posts=["2025/03/19/2/","2025/03/18/hello-world/","2025/03/19/Hexo常用命令/","2025/03/19/使用极空间部署MP/","2025/03/18/测试文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
