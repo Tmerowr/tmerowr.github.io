51862d9d1abf36f0d135e68e839d0e87108abd7a
tmerowr <tmerowr@gmail.com>
2025-03-25 18:58:47 +0800

    Site updated: 2025-03-25 18:58:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/19/Hexo常用命令/","2025/03/18/hello-world/","2025/03/19/使用极空间部署MP/","2025/03/19/修复Github与Vercel数据不同步/","2025/03/18/测试文章/"];function toRandomPost(){
+var posts=["2025/03/19/Hexo基础使用介绍/","2025/03/18/hello-world/","2025/03/19/使用极空间部署MP/","2025/03/19/修复Github与Vercel数据不同步/","2025/03/18/测试文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
