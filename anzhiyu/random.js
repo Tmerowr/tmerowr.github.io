1a5fb1694e4f80623cc7792dc2793a4ff7e8610a
tmerowr <tmerowr@gmail.com>
2025-04-07 11:45:34 +0800

    Site updated: 2025-04-07 11:45:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/19/Hexo基础使用介绍/","2025/03/29/Twikoo静态网站评论系统/","2025/03/18/hello-world/","2025/03/19/使用极空间部署MP/","2025/03/29/使用极空间部署兰空图床/","2025/03/19/修复Github与Vercel数据不同步/","2025/04/02/好玩的PT常用词汇/","2025/03/18/测试文章/"];function toRandomPost(){
+var posts=["2025/03/29/Twikoo静态网站评论系统/","2025/03/19/Hexo基础使用介绍/","2025/03/18/hello-world/","2025/03/19/使用极空间部署MP/","2025/03/19/修复Github与Vercel数据不同步/","2025/03/29/使用极空间部署兰空图床/","2025/03/18/测试文章/","2025/04/02/好玩的PT常用词汇/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
